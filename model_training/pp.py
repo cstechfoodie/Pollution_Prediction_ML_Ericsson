aefb7fad8e79de690d9dc9c92a8a2dd881547cbe
Shunyu Wang <shunyu878@gmail.com>
2019-06-01 11:44:49 -0400

    initialize variables

--- model_training/pp.py
+++ model_training/pp.py
@@ -1,1 +1,16 @@
 # 2019 Hackathon ML Ericsson
+
+csv_records_list = []
+training_csv_filepath = './challenge2_sorted.csv'
+trained_weights_file = "weights.txt"
+test_result_filepath_prefix = "./test_"
+
+learning_rate = 0.05
+threshold = 15
+w_DEWP, w_TEMP, w_PRES, w_cbwd, w_iws, w_is, w_ir = 0,0,0,0,0,0,0
+predicated_pm_for_next_hour = 0
+current_pm_for_this_hour = 0
+year = 0
+month = 0
+day = 0
+hour = 0
